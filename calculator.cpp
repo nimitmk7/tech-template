0a53445642461050cecb96d3b6b3febf892d0b8c
MatthewMa929 <MatthewMa929@Gmail.com>
2025-02-15 15:55:30 -0500

    divide

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,21 @@
 #include "Calculator.h"
+#include <stdexcept>
 
-int Calculator::add(int a, int b) { return a + b; }
-int Calculator::subtract(int a, int b) { return a - b; }
-int Calculator::multiply(int a, int b) { return a * b; }
+int Calculator::add(int a, int b) { 
+    return a + b; 
+}
+
+int Calculator::subtract(int a, int b) { 
+    return a - b; 
+}
+
+int Calculator::multiply(int a, int b) { 
+    return a * b; 
+}
+
+int Calculator::divide(int a, int b) {
+    if (b == 0) {
+        throw std::invalid_argument("Division by zero");
+    }
+    return a / b;
+}
--- calculator.h
+++ calculator.h
@@ -6,6 +6,7 @@ class Calculator {
         int add(int a, int b);
         int subtract(int a, int b);
         int multiply(int a, int b);
+        int divide(int a, int b);
     };
 
     
